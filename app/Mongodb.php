f19afa2596f517815ea15a29bd3170c22d34a470
CyberSai <isaacsai030@gmail.com>
2020-02-18 00:55:16 +0000

    Removing Appended ID

--- app/Mongodb.php
+++ app/Mongodb.php
@@ -10,22 +10,4 @@ class Mongodb extends Model
     protected $connection = 'mongodb';
 
     protected $fillable = ['name', 'email', 'password'];
-
-    public function toJson($options = 0)
-    {
-        $json = json_encode($this->toArray(), $options);
-
-        if (JSON_ERROR_NONE !== json_last_error()) {
-            throw JsonEncodingException::forModel($this, json_last_error_msg());
-        }
-
-        return $json;
-    }
-
-    public function toArray()
-    {
-        return array_merge(parent::toArray(),[
-            'id' => $this->_id
-        ]);
-    }
 }

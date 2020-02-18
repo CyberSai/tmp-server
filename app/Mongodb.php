a69e772bf2bce82939bbb8ceff0f20acf348d564
CyberSai <isaacsai030@gmail.com>
2020-02-18 00:52:08 +0000

    Mongo Adding Id

--- app/Mongodb.php
+++ app/Mongodb.php
@@ -2,13 +2,30 @@
 
 namespace App;
 
+use Illuminate\Database\Eloquent\JsonEncodingException;
 use Jenssegers\Mongodb\Eloquent\Model;
 
 class Mongodb extends Model
 {
-    protected $primaryKey = 'id';
-
     protected $connection = 'mongodb';
 
     protected $fillable = ['name', 'email', 'password'];
+
+    public function toJson($options = 0)
+    {
+        $json = json_encode($this->toArray(), $options);
+
+        if (JSON_ERROR_NONE !== json_last_error()) {
+            throw JsonEncodingException::forModel($this, json_last_error_msg());
+        }
+
+        return $json;
+    }
+
+    public function toArray()
+    {
+        return array_merge(parent::toArray(),[
+            'id' => $this->_id
+        ]);
+    }
 }
